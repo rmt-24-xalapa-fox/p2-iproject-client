9212e634645972b28635fc76bb5831a02ccd7ca7
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-06-21 20:34:57 +0700

    setup 2

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp, markRaw } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia())
-app.use(router)
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
 
-app.mount('#app')
+app.use(router);
+app.use(pinia);
+
+app.mount("#app");

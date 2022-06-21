b0b7867ac85480bcb7fe36e18f9f1ae4c144d401
Ugi Rahmat Ziadi <rahmatziadiugi@gmail.com>
2022-06-21 23:19:30 +0700

    init skeleton code

---
+++ src/stores/index.js
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+
+export const useMainStore = defineStore({
+  id: "main",
+  state: () => ({
+    transforms: [], // array of ofject pokemon, { id, name, level, hp, attack, deff, type, moves:[{id, name, type, power}] }
+    pokedex: [], // id/tag of pokemon encounter
+    tm: [], // array of object, 
+    potion: [], // array of object
+    money: 0,
+    enemies: [],
+    rounds: 0,
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2,
+  },
+  actions: {
+    getMaps(){
+      // random map to select
+      // include rounds for bonus map
+
+      // go legendary when round meets
+    },
+
+    getEnemies(){
+
+    },
+
+    getPokemons(){
+
+    },
+
+    getPokemonById(id){
+
+    },
+
+  },
+  created(){
+    // check if login or not
+    // if login get from db for state
+    // if no login check local storage again for "saved"
+  },
+});

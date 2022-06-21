afa7e3ebf31046e3dea9a1a5d515a490eda225fe
muhammadfaiq <faiq.tantyo@gmail.com>
2022-06-21 21:34:11 +0700

    template

---
+++ client/src/components/ContentCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "ContentCard",
+};
+</script>
+<template>
+  <div class="card text-center mb-3" style="height: 17vh; width: 100%">
+    <div class="card-body">
+      <h5 class="card-title">Title: <i>Test</i></h5>
+    </div>
+    <div class="div card-footer d-flex justify-content-end border-1 bg-footer">
+      <div>
+        <button class="button" style="width: 150px">Read More</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* content card */
+.bg-footer {
+  background-color: white;
+}
+
+.button {
+  transition-duration: 0.5s;
+  background-color: white;
+  height: 40px;
+  width: 100%;
+  border-radius: 5px;
+  color: #292828;
+  border-color: #292828;
+}
+
+.button:hover {
+  background-color: #292828; /* Green */
+  color: white;
+  border: none;
+}
+</style>

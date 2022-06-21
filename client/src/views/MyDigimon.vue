35a987fa10030c2669d17274cb808ef06fa72d7e
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-06-21 22:51:25 +0700

    show list my digimon forgot to branch

--- client/src/views/MyDigimon.vue
+++ client/src/views/MyDigimon.vue
@@ -12,6 +12,28 @@
       </div>
     </div>
   </div>
+
+  <div
+    class="container-xl text-center page-heading align-items-center d-flex justify-content-start border border-dark border-4 rounded"
+    style="background-color: white"
+  >
+    <!-- asdasdas -->
+    <div class="row justify-content-start">
+      <div
+        v-for="digimon in listMyDigimon"
+        :key="digimon.name"
+        class="card col-1"
+        style="width: 165px; margin: 10px"
+      >
+        <img class="card-img-top" :src="digimon.img" alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title retrofont" style="font-size: 0.7em">
+            {{ digimon.name }}
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { mapState, mapActions } from "pinia";

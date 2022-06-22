be53a7cd8082893299e9a41f85d9733b3fe7f635
ridhokhalis99 <ridhokholis9a@gmail.com>
2022-06-22 23:03:40 +0700

    add: readPalettes method and copy function for PaletteCard

--- src/stores/main.js
+++ src/stores/main.js
@@ -5,12 +5,24 @@ export const useMainStore = defineStore("main", {
   state: () => ({
     isLogin: false,
     colorPalette: [],
+    colorPalettes: [],
   }),
   actions: {
     checkIsLogin() {
       const access_token = localStorage.getItem("access_token");
       if (access_token) {
         this.isLogin = true;
+      }
+    },
+    async readPalettes() {
+      try {
+        const headers = {
+          access_token: localStorage.getItem("access_token"),
+        };
+        const res = await axiosInstance.get("/palettes", { headers });
+        this.colorPalettes = res.data;
+      } catch (err) {
+        console.log(err);
       }
     },
     async generateColor(colorScheme) {

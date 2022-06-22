9f7910a7916e6eb2842eb27eeebe91919a735487
nourmarafita <nmarafita@gmail.com>
2022-06-22 11:34:57 +0700

    fetch product

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="body-content outer-top-vs" id="top-banner-and-menu">
+  <div class="body-content outer-top-vs ml-4" id="top-banner-and-menu">
     <div class="container">
       <div class="row">
-        <Sidebar />
+        <!-- <Sidebar /> -->
         <!--==============================================CONTENT==============================================-->
-        <div class="col-xs-12 col-sm-12 col-md-9 homebanner-holder">
+        <div class="col-xs-12 col-sm-12 col-md-12 homebanner-holder">
           <!-- ========================================== SECTION – HERO ========================================= -->
 
           <div id="hero">
-            <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
+            <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm mb-4" style="margin-bottom: 100px;">
               <div class="item"
                 style="background-image: url(https://res.cloudinary.com/dzosnc60i/image/upload/v1655815975/abeille/01_khtci1.jpg);">
                 <div class="container-fluid">
@@ -49,7 +49,6 @@
 
           <!-- ========================================= SECTION – HERO : END ========================================= -->
 
-          <Product :products="products" />
 
           <!-- ============================================== WIDE PRODUCTS ============================================== -->
           <div class="wide-banners outer-bottom-xs">
@@ -134,186 +133,15 @@
           <!-- /.sidebar-widget -->
           <!-- ============================================== BEST SELLER : END ============================================== -->
 
-          <!-- ============================================== BLOG SLIDER ============================================== -->
-          <section class="section latest-blog outer-bottom-vs">
-            <h3 class="section-title">Latest form Blog</h3>
-            <div class="blog-slider-container outer-top-xs">
-              <div class="owl-carousel blog-slider custom-carousel">
-                <div class="item">
-                  <div class="blog-post">
-                    <div class="blog-post-image">
-                      <div class="image"> <a href="blog.html"><img src="../assets/images/blog-post/blog_big_01.jpg"
-                            alt=""></a> </div>
-                    </div>
-                    <!-- /.blog-post-image -->
-
-                    <div class="blog-post-info text-left">
-                      <h3 class="name"><a href="#">Voluptatem accusantium doloremque laudantium</a></h3>
-                      <span class="info">By Jone Doe &nbsp;|&nbsp; 21 March 2016 </span>
-                      <p class="text">Sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-                        aliquam
-                        quaerat voluptatem.</p>
-                    </div>
-                    <!-- /.blog-post-info -->
-
-                  </div>
-                  <!-- /.blog-post -->
-                </div>
-                <!-- /.item -->
-
-                <div class="item">
-                  <div class="blog-post">
-                    <div class="blog-post-image">
-                      <div class="image"> <a href="blog.html"><img src="../assets/images/blog-post/blog_big_02.jpg"
-                            alt=""></a> </div>
-                    </div>
-                    <!-- /.blog-post-image -->
-
-                    <div class="blog-post-info text-left">
-                      <h3 class="name"><a href="#">Dolorem eum fugiat quo voluptas nulla pariatur</a></h3>
-                      <span class="info">By Saraha Smith &nbsp;|&nbsp; 21 March 2016 </span>
-                      <p class="text">Sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-                        aliquam
-                        quaerat voluptatem.</p>
-                    </div>
-                    <!-- /.blog-post-info -->
-
-                  </div>
-                  <!-- /.blog-post -->
-                </div>
-                <!-- /.item -->
-
-                <!-- /.item -->
-
-                <div class="item">
-                  <div class="blog-post">
-                    <div class="blog-post-image">
-                      <div class="image"> <a href="blog.html"><img src="../assets/images/blog-post/blog_big_03.jpg"
-                            alt=""></a> </div>
-                    </div>
-                    <!-- /.blog-post-image -->
-
-                    <div class="blog-post-info text-left">
-                      <h3 class="name"><a href="#">Dolorem eum fugiat quo voluptas nulla pariatur</a></h3>
-                      <span class="info">By Saraha Smith &nbsp;|&nbsp; 21 March 2016 </span>
-                      <p class="text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
-                    </div>
-                    <!-- /.blog-post-info -->
-
-                  </div>
-                  <!-- /.blog-post -->
-                </div>
-                <!-- /.item -->
-
-                <div class="item">
-                  <div class="blog-post">
-                    <div class="blog-post-image">
-                      <div class="image"> <a href="blog.html"><img src="../assets/images/blog-post/blog_big_01.jpg"
-                            alt=""></a> </div>
-                    </div>
-                    <!-- /.blog-post-image -->
-
-                    <div class="blog-post-info text-left">
-                      <h3 class="name"><a href="#">Dolorem eum fugiat quo voluptas nulla pariatur</a></h3>
-                      <span class="info">By Saraha Smith &nbsp;|&nbsp; 21 March 2016 </span>
-                      <p class="text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
-                    </div>
-                    <!-- /.blog-post-info -->
-
-                  </div>
-                  <!-- /.blog-post -->
-                </div>
-                <!-- /.item -->
-
-                <div class="item">
-                  <div class="blog-post">
-                    <div class="blog-post-image">
-                      <div class="image"> <a href="blog.html"><img src="../assets/images/blog-post/blog_big_02.jpg"
-                            alt=""></a> </div>
-                    </div>
-                    <!-- /.blog-post-image -->
-
-                    <div class="blog-post-info text-left">
-                      <h3 class="name"><a href="#">Dolorem eum fugiat quo voluptas nulla pariatur</a></h3>
-                      <span class="info">By Saraha Smith &nbsp;|&nbsp; 21 March 2016 </span>
-                      <p class="text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
-                    </div>
-                    <!-- /.blog-post-info -->
-
-                  </div>
-                  <!-- /.blog-post -->
-                </div>
-                <!-- /.item -->
-
-              </div>
-              <!-- /.owl-carousel -->
-            </div>
-            <!-- /.blog-slider-container -->
-          </section>
-          <!-- /.section -->
-          <!-- ============================================== BLOG SLIDER : END ============================================== -->
-
-          <!-- ============================================== FEATURED PRODUCTS ============================================== -->
-          
-          <!-- /.section -->
-          <!-- ============================================== FEATURED PRODUCTS : END ============================================== -->
-
         </div>
         <!-- /.homebanner-holder -->
         <!-- ============================================== CONTENT : END ============================================== -->
       </div>
       <!-- /.row -->
-      <!-- ============================================== BRANDS CAROUSEL ============================================== -->
-      <div id="brands-carousel" class="logo-slider">
-        <div class="logo-slider-inner">
-          <div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
-            <div class="item m-t-15"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand1.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item m-t-10"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand2.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand3.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand4.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand5.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand6.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand2.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand4.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand1.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-
-            <div class="item"> <a href="#" class="image"> <img data-echo="../assets/images/brands/brand5.png"
-                  src="../assets/images/blank.gif" alt=""> </a> </div>
-            <!--/.item-->
-          </div>
-          <!-- /.owl-carousel #logo-slider -->
-        </div>
-        <!-- /.logo-slider-inner -->
-
-      </div>
       <!-- /.logo-slider -->
       <!-- ============================================== BRANDS CAROUSEL : END ============================================== -->
+
+      <Product :products="products" />
     </div>
     <!-- /.container -->
   </div>

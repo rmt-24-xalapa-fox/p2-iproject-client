354835acf6404ce87a50072fe57a4abdd56299c7
YOGA <YOGA@DESKTOP-TQHRCDL>
2022-06-23 11:33:53 +0700

    change user template

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -67,7 +67,7 @@ export default {
                                 <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png"
                                         alt="sunil"> </div>
                                 <div class="chat_ib">
-                                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                    <h5>User Test <span class="chat_date">Dec 25</span></h5>
                                     <p>Test, which is a new approach to have all solutions
                                         astrology under one roof.</p>
                                 </div>

b3bd6ee1d8bbffc80f4349554af29cdcc42d9e5d
lordRedsky <aminmuhammadrizki@gmail.com>
2022-06-24 17:21:06 +0800

    bug in copy link gift

--- client/src/views/GIF.vue
+++ client/src/views/GIF.vue
@@ -17,6 +17,10 @@ export default {
   },
   methods: {
     ...mapActions(useMaarvelStore, ["input"]),
+    copyUrl() {
+      const copy = this.$refs.url.select();
+      document.execCommand("copy", false, copy);
+    },
   },
 
   mounted() {
@@ -33,10 +37,18 @@ export default {
 <template>
   <div class="gift-container">
     <div class="gift" v-for="gif in gifs" :key="gif.id">
-    <div>
       <img class="gift-display" :src="gif.images.original.url" alt="" />
-      <input class="gift-display" type="text" />
-    </div>
+      <div>
+        <form action="">
+          <input
+            class="copy-gift"
+            type="text"
+            ref="url"
+            :value="`${gif.images.original.url}`"
+          />
+          <button class="copy-btn" v-on:click="copyUrl">Copy</button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -46,13 +58,44 @@ export default {
   margin-top: 300px;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
+}
+
+.gift {
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+}
+
+button.copy-btn {
+  margin-top: 10px;
+  background: #e54040;
+  color: #fff;
+  border: 0;
+  padding: 5px;
+  border-radius: 3px;
+  cursor: pointer;
+  width: 21%;
+  height: 40px;
+  font-size: 20px;
 }
 
 img.gift-display {
-  width: 300px;
-  height: 300px;
+  width: 320px;
+  height: 320px;
   margin: 10px;
+}
+
+input.copy-gift {
+  margin: 10px;
+  width: 70%;
+  height: 40px;
+  padding: 10px 5px;
+  float: left;
+  font: bold 17px "lucida sans", "trebuchet MS", "Tahoma";
+  border: 0;
+  background: #eee;
+  -moz-border-radius: 3px 0 0 3px;
+  -webkit-border-radius: 3px 0 0 3px;
+  border-radius: 5px;
 }
 </style>
